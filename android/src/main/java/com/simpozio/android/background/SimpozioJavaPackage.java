4c90e395510ff6aa4e83e492a1719c77e018385d
Vadim Patsev <vadim@smekalka.com>
2018-05-20 14:10:16 +0300

    SimpozioJavaPackage

---
+++ android/src/main/java/com/simpozio/android/background/SimpozioJavaPackage.java
@@ -0,0 +1,30 @@
+package com.simpozio.android.background;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SimpozioJavaPackage implements ReactPackage {
+
+    @Override
+    public List<NativeModule> createNativeModules(
+            ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+
+        modules.add(new SimpozioJavaService(reactContext));
+
+        return modules;
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(
+            ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+
+}
